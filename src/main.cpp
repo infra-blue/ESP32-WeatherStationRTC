a6a684f9254a956d3a6897586388309ee2a968a9
Leonardo Mirabella <mrblrd02p28c351u@studium.unict.it>
2024-06-18 14:57:56 +0200

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,11 @@
 #include <NTPClient.h>
 #include <Timezone.h>
 #include <string>
-#include <iostream>
+#include <sstream>
 #include "Font_Data.h"
+
+#define SSID "clock"
+#define PASSWORD "coglione1"
 
 #define CLK_PIN   12
 #define DATA_PIN  11
@@ -18,14 +21,11 @@
 #define MAX_DEVICES 4
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 
-char ssid[] = "clock";
-char password[] = "coglione1";
-
 MD_Parola matrix = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 RTC_DS3231 rtc;
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "time.windows.com");
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
 TimeChangeRule CET = {"CET", Last, Sun, Oct, 3, 60};    // Daylight time = UTC + 1 hour
 TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     // Standard time = UTC + 2 hours
@@ -47,7 +47,7 @@ void setup()
 
   if (rtc.lostPower()) {
     WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
+    WiFi.begin(SSID, PASSWORD);
 
     while(WiFi.status() != WL_CONNECTED)
       delay(100);
@@ -68,15 +68,17 @@ void setup()
 
 void loop()
 {
-  char hh_mm[6];
-  char ss[3];
-
-  /*
-    picks up the current time from RTC (which is in DateTime class)
-    converts it to epochtime
-    then to local time and then to DateTime class
-  */
+  /**
+   * @brief
+   *  picks up the current time from RTC (which is in DateTime class)
+   *  converts it to epochtime
+   *  then to local time and then to DateTime class
+   * 
+   * @param DateTime
+   */
   DateTime now = DateTime(CE.toLocal((rtc.now()).unixtime()));
+
+  char hh_mm[6], ss[3];
 
   sprintf(hh_mm, "%02d%c%02d", now.hour(), ((now.second() % 2) ? ':' : ' '), now.minute());
   sprintf(ss, "%02d", now.second());
@@ -87,12 +89,13 @@ void loop()
   matrix.setFont(0, small_num);
   matrix.setFont(1, pixel_font);
 
-  matrix.displayZoneText(0, ss, PA_LEFT, 75, 0, PA_PRINT, PA_NO_EFFECT);
-  matrix.displayZoneText(1, hh_mm, PA_CENTER, 75, 0, PA_PRINT, PA_NO_EFFECT);
+  if(matrix.displayAnimate()) {
+    matrix.displayZoneText(0, ss, PA_LEFT, 75, 0, PA_PRINT, PA_NO_EFFECT);
+    matrix.displayZoneText(1, hh_mm, PA_CENTER, 75, 0, PA_PRINT, PA_NO_EFFECT);
 
-  matrix.displayAnimate();
-  matrix.displayReset(0);
-  matrix.displayReset(1);
+    matrix.displayReset(0);
+    matrix.displayReset(1);
+  }
 }
 
 /*
