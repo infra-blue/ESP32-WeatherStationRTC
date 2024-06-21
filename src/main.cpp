6d50ee3f0bf41bd4af249a1ea835339a611be55a
Leonardo Mirabella <leonardomirabella1@gmail.com>
2024-06-21 12:15:44 +0200

    feat: added screens functions

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
 #include <Font_Data.h>
 #include <Local_Time.h>
 
+#include <Screens.h>
+
 MD_Parola matrix = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 RTC_DS3231 rtc;
 
@@ -37,24 +39,21 @@ void setup()
 
     WiFi.mode(WIFI_STA);
 
-    Serial.println("Scan start.\n");
+    Serial.printf("Scan start.\n");
 
     int n_net = WiFi.scanNetworks();
     bool ap_found = false;
-  
-    if (n_net) {
-        for (int i = 0; i < n_net; ++i) {
-          Serial.println(WiFi.SSID(i));
+
+    if (n_net)
+       for (int i = 0; i < n_net; ++i)
           if(WiFi.SSID(i) == YOUR_SSID) {
             ap_found = true;
             break;
-          }
-        }
-    }
-
-    Serial.println("AP found.\n");
+         }
 
     if(ap_found) {
+      Serial.printf("AP found.\n");
+
       WiFi.begin(YOUR_SSID, YOUR_PASSWORD);
 
       Serial.printf("Connecting to WiFi.\n");
@@ -69,51 +68,29 @@ void setup()
       timeClient.update();
       delay(500);
 
-      if(timeClient.isTimeSet())
+      if(timeClient.isTimeSet()) {
         rtc.adjust(timeClient.getEpochTime());
+        Serial.printf("RTC adjusted with NTP time.\n Disconnecting from WiFi.\n");
+      }
+      else {
+        Serial.printf("NTP server error. Time not set.\n");
+        rtc.adjust(DateTime("Jan 1 2000", "00:00:00"));
+      }
 
       WiFi.disconnect();
-
-      Serial.printf("RTC adjusted with NTP time.\n Disconnecting from WiFi.\n");
     }
     else {
       Serial.printf("WiFi AP not found. Cannot Sync time.\n");
       rtc.adjust(DateTime("Jan 1 2000", "00:00:00"));
     }
   }
+
   matrix.setIntensity(10);
 }
 
 void loop()
 {
-  /**
-   * @brief
-   *  picks up the current time from RTC (which is in DateTime class)
-   *  converts it to epochtime
-   *  then to local time and then to DateTime class
-   * 
-   * @param DateTime
-   */
-  DateTime now = DateTime(current_timezone->toLocal((rtc.now()).unixtime()));
-
-  char hh_mm[6], ss[3];
-
-  sprintf(hh_mm, "%02d%c%02d", now.hour(), ((now.second() % 2) ? ':' : ' '), now.minute());
-  sprintf(ss, "%02d", now.second());
-
-  matrix.setZone(0, 0, 0);
-  matrix.setZone(1, 1, 3);
-
-  matrix.setFont(0, font[0]);
-  matrix.setFont(1, font[1]);
-
-  if(matrix.displayAnimate()) {
-    matrix.displayZoneText(0, ss, PA_LEFT, 75, 0, PA_PRINT, PA_NO_EFFECT);
-    matrix.displayZoneText(1, hh_mm, PA_CENTER, 75, 0, PA_PRINT, PA_NO_EFFECT);
-
-    matrix.displayReset(0);
-    matrix.displayReset(1);
-  }
+  print_time(matrix, rtc);
 }
 
 /*
